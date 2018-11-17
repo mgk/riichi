4a0fe7e191afda791b5bd2c167636621dc2dca4b
Michael Keirnan <michael@keirnan.com>
2018-11-17 11:46:44 -0500

    arrangements working

--- lib/riichi/tiles.rb
+++ lib/riichi/tiles.rb
@@ -23,6 +23,10 @@ module Riichi
       Tiles.new(tiles)
     end
 
+    def self.diff(a, b)
+      (Tiles.new(a) - Tiles.new(b)).tiles
+    end
+
     def -(other)
       result = tiles.dup
       other.tiles.each do |t|
@@ -43,15 +47,11 @@ module Riichi
       self.pung? tiles
     end
 
-    def self.chow?(tiles)
-      tiles.size == 3 &&
-      tiles.all? { |tile| tile.suited? } &&
-      tiles.all? { |tile| tile.suit == tiles[0].suit } &&
-      tiles[1].rank == tiles[0].rank + 1 &&
-      tiles[2].rank == tiles[1].rank + 1
+    def self.chow(tile)
+      [tile, tile.next_in_suit, tile.next_in_suit.next_in_suit]
     end
 
-    def self.chow_start?(tiles)
+    def self.initial_chow(tiles)
       sequence_length = 1
       curr_idx = 0
       next_idx = 1
@@ -69,17 +69,32 @@ module Riichi
           curr_idx = next_idx
           next_idx += 1
           if sequence_length == 3
-            return true
+            return chow(tiles[0])
           end
         else
-          return false
+          return nil
         end
       end
 
-      if tiles[1] != first
-        return false
-      end
+      nil
 
+    end
+
+    def self.initial_pung(tiles)
+      pung = tiles[0, 3]
+      pung if self.pung?(pung)
+    end
+
+    def self.initial_sets(tiles)
+      [self.initial_chow(tiles), self.initial_pung(tiles)].compact
+    end
+
+    def self.chow?(tiles)
+      tiles.size == 3 &&
+      tiles.all? { |tile| tile.suited? } &&
+      tiles.all? { |tile| tile.suit == tiles[0].suit } &&
+      tiles[1].rank == tiles[0].rank + 1 &&
+      tiles[2].rank == tiles[1].rank + 1
     end
 
     def chow?
@@ -95,23 +110,69 @@ module Riichi
     end
 
     def sets
-      _sets([], @tiles)
+      []
     end
 
-    private def _sets(acc, remaining)
+    def self.matches?(set, tile)
+      set.empty? ||
+      (set.length == 1 && tile == set.last || tile == set.last.next_in_suit) ||
+      Tiles.set?(set + [tile])
+    end
+
+    def self.arr(str)
+      _arr(0, [], [], Tiles.from_s(str).tiles)
+    end
+
+    # acc - list of arrangements
+    # arrangement - list of sets
+    # set - list of tiles
+    def self._arr(level, acc, arrangement, remaining)
+      puts
+      pr = lambda do |s|
+        if level == 0
+          puts ("(#{level}) " + "    " * level) + s
+        end
+      end
+      pr.call("       acc: #{acc.to_tile_strings}")
+      pr.call("arrangement: #{arrangement.to_tile_strings}")
+      pr.call(" remaining: #{remaining.to_tile_strings}")
       if remaining.empty?
-        return acc
+        pr.call("      ans1: #{acc.to_tile_strings}")
+        return acc + [arrangement]
       end
 
-      candidate = remaining.take(3)
-      rest = remaining.drop(1)
+      isets = initial_sets(remaining)
+      pr.call("     isets: #{isets.to_tile_strings}")
 
-      if Tiles.set?(candidate)
-        _sets(acc + [candidate], rest)
+      combos = isets.map {|c| [c, Tiles.diff(remaining, c)]}
+      pr.call("    combos: #{combos.to_tile_strings}")
+
+      if combos.empty?
+        pr.call("      empty")
+        return _arr(level + 1, acc, arrangement, remaining.drop(1))
       else
-        _sets(acc, rest)
+        ans = combos.flat_map do |set, rest|
+          pr.call("     combo: #{set.to_tile_strings}--#{rest.to_tile_strings}")
+          cans = _arr(level + 1, acc, arrangement + [set], rest)
+          pr.call("combo ans: #{cans.to_tile_strings}")
+          cans
+        end
+
+        pr.call("      ans2: #{ans.to_tile_strings}")
+        return ans
       end
     end
 
+  end
+
+  class Arr
+    attr_accessor :sets, :strays
+    def initialize(sets, strays)
+      @sets = sets
+      @strays = strays
+    end
+    def to_s
+      "sets: #{sets.to_tile_strings}, strays: #{strays.to_tile_strings}"
+    end
   end
 end
--- main.rb
+++ main.rb
@@ -10,6 +10,38 @@ require 'riichi'
 # ["1p 3p 9p 3s 7s 8s 8s 8s 9s 1m 2m 3m 6m S",
 #   ["7s 8s 9s", "8s 8s 8s", "1m 1m 3m"]]
 
-t2 = "5p 7p 8p 9p 1s 1s 3s 5s 1m 2m 3m 3m 4m N"
-tiles = Riichi::Tiles.from_s(t2)
-puts tiles.sets
+class Array
+  def to_tile_strings
+    self.map do |x|
+      case x
+      when Riichi::Tile then x.to_s
+      when Array then x.to_tile_strings
+      else x
+      end
+    end
+  end
+end
+
+[
+  "1s 1s 1s 2s 2s 2s",
+  "1s 1s 1s 2s 3s",
+  "1s 1s 1s 2s 3s 5m 6m 7m",
+  "1s 1s 1s 2s 3s 4s N",
+].each do |s|
+  puts "Tiles: #{s}"
+  arrangements = Riichi::Tiles.arr(s)
+  # p x.to_tile_strings
+  puts "==>"
+  arrangements.each_with_index do|arr, i|
+    puts "arrangement <#{i}>"
+    arr.each do |set|
+      puts "   set: #{set.to_tile_strings.inspect}"
+    end
+  end
+  puts "--\n\n"
+end
+
+# t2 = "5p 7p 8p 9p 1s 1s 3s 5s 1m 2m 3m 3m 4m N"
+# tiles = Riichi::Tiles.from_s(t2)
+# puts "Computing sets for #{tiles}"
+# puts tiles.sets.map { |a| "[#{a.join', '}]"}
